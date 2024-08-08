d489398e54fb2a20fa0960ba268bf5cafc9a47a7
Hanru Vorster <hanru.vorster@gmail.com>
2024-08-08 09:21:20 +0200

    added genre preferences for Drax, Rocket, Groot in the guardians object

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "Rock",
+    "Rocket": "R&B",
+    "Groot": "Pop"
     // Add preferences for Drax, Rocket, and Groot
 };
 
